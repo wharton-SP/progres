7b3b7c6b1699a934d6424077bd50233cf3ba460b
dzyanino <dzyanino@gmail.com>
2025-07-22 16:57:21 +0300

    Refactor : moved ui related files to `app/ui/`

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_MainWindow
+from app.ui.pyui.ui_main import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
